703fab4df00e2d8bd6a7d2989cf34ca51757cc07
Ekow-Davis <paakowdavis@gmail.com>
2025-08-09 11:32:22 +0000

    task 1 complete

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog_1 = new Dog();
+        dog_1.MakeSound();
+        Cat cat_1 = new Cat();
+        cat_1.MakeSound();
+    }
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+
+
+    
+}
