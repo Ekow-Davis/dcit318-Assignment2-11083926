0c078955819213fd3fb749160fd52416f25126de
Ekow-Davis <paakowdavis@gmail.com>
2025-08-09 11:35:24 +0000

    task 2 complete

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Circle circle1 = new Circle();
+        circle1.radius = 7;
+        circle1.getArea();
+
+        pie = 3.142;
+
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.height = 4;
+        rectangle1.length = 2;
+        rectangle1.getArea();
+
+
+
+    }
+
+    public abstract class Shape
+    {
+        public abstract void getArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double radius;
+
+        public override void getArea()
+        {
+            double area = pie * (radius * radius);
+            Console.WriteLine(area);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double length;
+        public double height;
+
+        public override void getArea()
+        {
+            double area = height * length;
+            Console.WriteLine(area);
+        }
+
+    }
+
+
+}
